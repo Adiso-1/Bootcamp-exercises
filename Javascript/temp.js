d47de9647c581a47c17a23c191f8f344e9d6af75
Adi Levy <levyadi93@gmail.com>
2021-02-22 22:51:29 +0200

    exercises

--- Javascript/temp.js
+++ Javascript/temp.js
@@ -1,47 +1,5 @@
-function humanReadable(seconds) {
-  let array = [];
-  if (Math.floor(seconds / 3600) > 0) {
-    array.push(Math.floor(seconds / 3600).toLocaleString("en-US", {
-    minimumIntegerDigits: 2,
-    useGrouping: false,
-  }))
-  seconds -= Math.floor(seconds / 3600) * 3600;
-  } else {
-    array.push('00')
-  }
-  console.log(seconds);
-  if (Math.floor(seconds / 60) > 0) {
-    array.push(
-      Math.floor(seconds / 60).toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      })
-    );
-    seconds -= Math.floor(seconds / 60) * 60;
-  } else {
-    array.push("00");
-  }
-  console.log(seconds);
-  if (seconds < 60) {
-    array.push(
-      Math.floor(seconds).toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      })
-    );
-  } else {
-    array.push("00");
-  }
-  return array.join(":");
+function validate(password) {
+  console.log(password)
+  return /(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{6,}/g.test(password);
 }
-console.log(humanReadable(359999));
-
-
-
-// [17, 7.5, -7.2345].forEach((myNumber) => {
-//   let formattedNumber = myNumber.toLocaleString("en-US", {
-//     minimumIntegerDigits: 2,
-//     useGrouping: false,
-//   });
-//   console.log("Input:    " + myNumber + "\n" + "Output:   " + formattedNumber);
-// });
+console.log(validate("fjd3IR9.;"));
