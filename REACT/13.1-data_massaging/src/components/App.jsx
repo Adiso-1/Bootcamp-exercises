3daac43b2cc29c523aec7c47fee749d3cbf69408
Adi Levy <levyadi93@gmail.com>
2021-03-21 15:38:26 +0200

    React 13.1 ex

---
+++ REACT/13.1-data_massaging/src/components/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import data from '../data';
+import Name from './Name';
+import Card from './Card';
+
+class App extends React.Component {
+	state = { names: [], over1990: [], isLoad: false };
+
+	names = () => {
+		const names = data.map((person) => person.name);
+		return this.setState({ names });
+	};
+
+	over1990 = () => {
+		const filtered = data.filter(
+			(person) => new Date(person.birthday).getFullYear() > 1990
+		);
+		return this.setState({ over1990: filtered });
+	};
+
+	componentDidMount() {
+		this.names();
+		this.over1990();
+		this.setState({ isLoad: true });
+	}
+
+	drawPerson = () => {
+		const myArr = this.state.over1990.map((person, i) => {
+			return <Card key={i} obj={person} />;
+		});
+		return myArr;
+	};
+	render() {
+		if (this.state.isLoad) {
+			return (
+				<div style={{ display: 'flex' }}>
+					<Name persons={this.state.name} />
+					{this.drawPerson()}
+				</div>
+			);
+		}
+		return null;
+	}
+}
+export default App;
