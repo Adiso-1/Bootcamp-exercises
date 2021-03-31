cd87a63324de58f09bd4d0a80d74c3d20b213f52
Adi Levy <levyadi93@gmail.com>
2021-03-31 22:14:34 +0300

    ex 21.1-editing

--- REACT/21.1-editing/src/App.js
+++ REACT/21.1-editing/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from 'react';
+import Button from './components/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [isEditClicked, setIsEditClicked] = useState(false);
+	const inputRef = useRef();
 
+	const onEditClick = () => setIsEditClicked(!isEditClicked);
+
+	useEffect(() => {
+		if (isEditClicked) {
+			inputRef.current.focus();
+		}
+	}, [isEditClicked]);
+	return (
+		<div>
+			{isEditClicked ? (
+				<div>
+					{' '}
+					<Button name="save" onClick={onEditClick} />
+					<input type="text" ref={inputRef} />
+				</div>
+			) : (
+				<Button name="edit" onClick={onEditClick} />
+			)}
+		</div>
+	);
+};
 export default App;
