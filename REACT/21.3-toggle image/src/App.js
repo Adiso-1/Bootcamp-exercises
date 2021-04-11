b2b7f551cdea351361017664be280d1604145413
Adi Levy <levyadi93@gmail.com>
2021-04-11 14:44:07 +0300

    node_vs_js ex.

--- REACT/21.3-toggle image/src/App.js
+++ REACT/21.3-toggle image/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const img1 =
+	'https://images.unsplash.com/photo-1610547169320-9c54714e4e73?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80';
+const img2 =
+	'https://images.unsplash.com/photo-1611657365907-1ca5d9799f59?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80';
+
+const App = () => {
+	const [isHover, setIsOver] = useState(false);
+	const imageRef1 = useRef();
+	const imageRef2 = useRef();
+
+	const overImg = (e) => {
+		e.target.style.filter = '';
+		setIsOver(true);
+	};
+
+	const outImg = (e) => {
+		e.target.style.filter = 'grayscale(1)';
+		setIsOver(false);
+	};
+
+	return (
+		<div>
+			<img
+				style={{ filter: 'grayscale(1)', margin: '10px' }}
+				ref={imageRef1}
+				onMouseOver={overImg}
+				onMouseOut={outImg}
+				src={img1}
+			/>
+			<img
+				style={{ filter: 'grayscale(1)', margin: '10px' }}
+				ref={imageRef2}
+				onMouseOver={overImg}
+				onMouseOut={outImg}
+				src={img2}
+			/>
+		</div>
+	);
+};
 
 export default App;
